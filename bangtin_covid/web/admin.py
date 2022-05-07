7e79b8f659ab8b5b2c298a6b14e1e51f8043a779
hoangtuyenblogger <hoangtuyenblogger@gmail.com>
2022-05-07 14:07:44 +0700

    update
    update

--- bangtin_covid/web/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import  covid_links_news , covid_news
-admin.site.register(covid_links_news)
-admin.site.register(covid_news)
--- bangtin_covid/web/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.home),
-    path('news/', views.news, name = 'search_news'),
-    path('post/', views.post, name = 'post'),
-]
